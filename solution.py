2c3ae2533a395dcefde9e3b2f2cd281e00fd1139
krustykrabfranchise <126911218+krustykrabfranchise@users.noreply.github.com>
2023-04-13 18:26:47 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,4 +9,4 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return sp.stats.anderson_ksamp([x, y]).pvalue < 0.03
+    return sp.stats.cramervonmises_2samp(x, y).pvalue < 0.03
